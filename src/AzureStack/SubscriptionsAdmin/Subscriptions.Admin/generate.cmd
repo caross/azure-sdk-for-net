8a330418e8dd71a21abd370230f305957eb592fa
Carl Ross <ross.carl@outlook.com>
2018-05-16 09:57:31 -0700

    Move subscription operation addition

--- src/AzureStack/SubscriptionsAdmin/Subscriptions.Admin/generate.cmd
+++ src/AzureStack/SubscriptionsAdmin/Subscriptions.Admin/generate.cmd
@@ -5,6 +5,6 @@
 
 @echo off
 rd Generated /S /Q
-call %~dp0..\..\..\..\tools\generate.cmd azsadmin/resource-manager/subscriptions latest azure master azure-rest-api-specs %CD%
+call %~dp0..\..\..\..\tools\generate.cmd azsadmin/resource-manager/subscriptions latest caross caross/moveSubscriptions azure-rest-api-specs  %CD%
 ::move /Y Subscriptions\Subscriptions.Admin\Generated .
 ::rd Subscriptions /S /Q
